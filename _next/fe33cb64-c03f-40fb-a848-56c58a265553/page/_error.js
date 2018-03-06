07b80292d62bbbdcbc7a9f1eedb69f3b233a480d
David Smith <dave@avaragado.org>
2018-03-06 12:29:00 +0000

    initial commit

---
+++ _next/fe33cb64-c03f-40fb-a848-56c58a265553/page/_error.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([28],{682:function(e,r,t){e.exports=t(683)},683:function(e,r,t){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(r,"__esModule",{value:!0});var u=t(0),o=function(e){if(e&&e.__esModule)return e;var r={};if(null!=e)for(var t in e)Object.prototype.hasOwnProperty.call(e,t)&&(r[t]=e[t]);return r.default=e,r}(u),a=t(422),f=t(76),i=n(f),l=t(522),s=n(l),c=function(e,r){var t=e||r,n=t.statusCode,u=void 0===n?500:n;return(0,a.makeSiteError)("status-error",u)},d=function(e){return o.createElement(s.default,e)};d.getInitialProps=function(e){var r=e.res,t=e.err;return{error:c(r,t)}},r.default=(0,i.default)({Page:d})}},[682]);
+            return { page: comp.default }
+          })
+        
